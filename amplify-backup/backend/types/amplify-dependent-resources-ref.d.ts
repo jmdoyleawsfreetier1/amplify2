cbd2c711cc230b99ace645f91d6f2fd55159c523
freetier <jmdoyle.aws.free.tier.1@gmail.com>
2023-06-13 13:39:52 -0400

    addded db

--- amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplify2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplify2eb2220f6": {
       "AppClientID": "string",
